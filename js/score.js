f17155629eb9ffa88449d2122de7d95356ad6776
Luca Biagetti <notciabaku@gmail.com>
2020-09-22 15:50:32 +0200

    Codici sconto diversificati.

--- js/score.js
+++ js/score.js
@@ -2,8 +2,14 @@ var max_score;
 setMaxScore();
 
 function endGame(point) {
-    if (point > MIN_POINT_CIUPON) {
-        showCodiceModal("10%", "codice");
+    if (point > 100 && point <= 150){
+        showCodiceModal("5€", "codice");
+    } if (point > 150 && point <= 200){
+        showCodiceModal("10€", "codice");
+    } if (point > 200 && point <= 400){
+        showCodiceModal("15€", "codice");
+    } if (point > 400){
+        showCodiceModal("20€", "codice");
     } else {
         showModal("modal-fallito");
     }
